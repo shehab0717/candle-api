22d31607f6e0791509f0cba5520269007fac5896
Shehab Abdullah <shehab.abdullah12@gmail.com>
2026-04-16 23:47:57 +0200

    update: post author reverse relation name

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -6,7 +6,11 @@ from common.models import TimestampedModel
 class Post(TimestampedModel):
     title = models.CharField(max_length=200)
     content = models.TextField(max_length=5000)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="posts",
+    )
 
     def __str__(self):
         return self.title
