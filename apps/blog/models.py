16780b91e7544764f0c24cf0bef4b122b2fcde2a
Shehab Abdullah <shehab.abdullah12@gmail.com>
2026-04-16 14:54:45 +0200

    add: post model

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
+from common.models import TimestampedModel
 
-# Create your models here.
+
+class Post(TimestampedModel):
+    title = models.CharField(max_length=200)
+    content = models.TextField(max_length=5000)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
