4a7b0c8500879d8ded6ac6be18bb27fc8cdce587
Shehab Abdullah <shehab.abdullah12@gmail.com>
2026-05-04 07:19:01 +0300

    update: posts url patterns

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -3,7 +3,7 @@ from .views import UserPostsView
 from . import views
 
 urlpatterns = [
-    path("posts/", views.PostList.as_view(), name="posts_list"),
-    path("posts/u/", UserPostsView.as_view(), name="user_posts"),
-    path("posts/<int:post_id>/", views.PostView2.as_view(), name="post_detail"),
+    path("", views.PostList.as_view(), name="posts_list"),
+    path("u/", UserPostsView.as_view(), name="user_posts"),
+    path("<int:post_id>/", views.PostView2.as_view(), name="post_detail"),
 ]
