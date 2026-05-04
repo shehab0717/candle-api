36b96f191d30625edb9a4e4a3badac9d61ab8ee4
Shehab Abdullah <shehab.abdullah12@gmail.com>
2026-05-04 06:14:52 +0300

    update: fetch post attachments in get:posts

--- apps/blog/serializers.py
+++ apps/blog/serializers.py
@@ -1,14 +1,25 @@
 from rest_framework import serializers
 
-from apps.blog.models import Post
+from apps.blog.models import Post, PostAttachment
 from apps.users.models import User
 from apps.users.serializers import GetUserSerializer
 
 
 class GetPostSerializer(serializers.ModelSerializer):
+    attachments = serializers.SerializerMethodField()
+
     class Meta:
         model = Post
         fields = "__all__"
+
+    def get_attachments(self, obj):
+        request = self.context.get("request")
+        return [
+            request.build_absolute_uri(attachment.file.url)
+            if request and attachment.file
+            else attachment.file.url
+            for attachment in obj.attachments.all()
+        ]
 
 
 class GetPostWithUserSerializer(GetPostSerializer):
@@ -24,13 +35,31 @@ class GetUserPostsSerializer(serializers.ModelSerializer):
 
 
 class CreatePostSerializer(serializers.ModelSerializer):
+    attachments = serializers.ListField(
+        child=serializers.FileField(),
+        write_only=True,
+        required=False,
+        help_text="List of files to attach to the post",
+    )
+
     class Meta:
         model = Post
         fields = "__all__"
         read_only_fields = ["author"]
 
+    def create(self, validated_data):
+
+        files = validated_data.pop("attachments", [])
+
+        post = Post.objects.create(**validated_data)
+
+        for file in files:
+            PostAttachment.objects.create(post=post, file=file)
+        return post
+
 
 class UpdatePostSerializer(serializers.ModelSerializer):
+    # TODO: update this serializer to allow attachments update
     class Meta:
         model = Post
         fields = ["title", "content"]
